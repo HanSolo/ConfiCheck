00a154815638e1c5d0e9650553f1d88bf917caf2
Gerrit Grunwald <han.solo@mac.com>
2025-01-15 16:26:09 +0100

    Initial Commit

---
+++ ConfiCheck/ConfiCheckApp.swift
@@ -0,0 +1,32 @@
+//
+//  ConfiCheckApp.swift
+//  ConfiCheck
+//
+//  Created by Gerrit Grunwald on 15.01.25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ConfiCheckApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
